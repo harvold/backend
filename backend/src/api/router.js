5f93db7cae44483844ace8ee3de2b5f5bbf78ef7
Tiger Kong <tig567899@users.noreply.github.com>
2018-07-01 00:34:24 -0700

    added get_pokemon function

--- backend/src/api/router.js
+++ backend/src/api/router.js
@@ -1,8 +1,34 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', function (req, res) {
-  res.send('hello world');
+const mysql = require('mysql');
+
+var con = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: "",
+	database: 'harvold'
+});
+
+con.connect(function(err)
+{
+	if (err) 
+		throw err;
+	console.log("MySQL Connected");
+});
+
+router.get('/get_pokemon', function (req, res) {
+	var user = "tig567899";
+	var sql = "SELECT name, hp, max_hp, exp, to_next FROM pokemon WHERE owner= ?";
+	con.query(sql, [user], function(err, result)
+	{
+		if (err) throw err;
+		console.log ("searched");
+		res.status(200).json(result);
+	});
 })
 
+router.get('/', function (req, res) {
+	res.send('hello world');
+})
 module.exports = router;
